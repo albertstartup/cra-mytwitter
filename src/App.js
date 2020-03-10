3a4cffe670c8dc5b8c50f3cf7c81e533e78ef181
Albert Martinez <albertstartup@gmail.com>
2020-03-09 20:10:28 -0700

    Add status form

--- src/App.js
+++ src/App.js
@@ -4,115 +4,122 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import Navbar from 'react-bootstrap/Navbar'
 import Card from 'react-bootstrap/Card'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Image from 'react-bootstrap/Image'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Navbar bg="dark" variant="dark">
-          Testing
-        </Navbar>
-      </header>
-      <main>
-        <Card style={{ width: '60%', margin: '0 auto'}}>
-          Test
-        </Card>
-      </main>
-      <footer>
-        <Navbar bg="dark" variant="dark">
-        </Navbar>
-      </footer>
-    </div>
-  );
-}
-
-export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    <Container fluid className="App">
+      <Row className="my-row">
+        <Col className="my-col site-header">1 of 1</Col>
+        <Col className="my-col">
+          <div className="middle-header">
+            Home
+          </div>
+          <div className="statusForm">
+            <Image className="statusForm-avatar" src="https://pbs.twimg.com/profile_images/1154877451966640128/e1lCYBdo_bigger.jpg" roundedCircle/>
+            <input className="statusForm-input" placeholder="What's happening?"/>
+            <div className="statusForm-foot">
+              <button className="my-button">Tweet</button>
+            </div>
+          </div>
+        </Col>
+        <Col className="my-col rigthBar">1 of 1</Col>
+      </Row>
+    </Container>
+    );
+  }
+  
+  export default App;
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
